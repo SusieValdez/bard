767e1f8d5d464d0a6200f04db719e0bc645867cb
Psiloclast <me@psiloclast.com>
2021-06-25 21:48:42 -0500

    first commit

---
+++ scripts/add_item.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const ITEM = process.env.ITEM;
+const FILENAME = "items.json";
+
+const oldData = JSON.parse(fs.readFileSync(FILENAME));
+const data = [
+  {
+    item: ITEM,
+    timestamp: new Date().toISOString(),
+  },
+  ...oldData,
+];
+fs.writeFileSync(FILENAME, JSON.stringify(data, null, 2));
